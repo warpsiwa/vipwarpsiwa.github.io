1fc0e5a9ff4cc421b49d59970a53df7b72fd51fd
furi diningrat <furiappid@gmail.com>
2020-05-25 14:39:27 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>buy VIP Member</title>
+    <meta name="google-site-verification" content="OKhQst1vyqBIHZ71FLlwutmhnPrdxUD7a1Ruu2pIZbs" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:800|Muli:400,800&display=swap" rel="stylesheet">
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="http://localhost/test/css.css"  media="screen,projection"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" type="image/png" sizes="32x32" href="http://localhost/test/icon.png">
+    <link rel="manifest" href="/images/ico/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="http://localhost/test/icon.png">
+    <meta name="theme-color" content="#ffffff">
+  </head>
+  
+  <body>
+    <main>
+      <div class="container">
+        <div class="row section">
+          <div class="col s12 flow-text">
+            <h3>Register VIP Member! (Indonesian Only)</h3></br>
+            <p>Cara bergabung VIP Member Warpsiwa Khusus untuk User Indonesia.</p>
+            <p>Anda hanya perlu membayar Rp250.000/250rb sekali Untuk mendapatkan Link <b>Google drive</b> Full Video Tanpa iklan,</p>
+            <p>Kelebihan bergabung dengan VIP member:</p>
+			<p> - Anda akan mendapatkan update video terbaru</p>
+			<p> - tidak perlu lagi melewati Short Link yang penuh iklan</p>
+			<p> - video kualitas Full HD, Durasi Panjang</p>
+			<br>
+			<img src="http://localhost/test/satu.png" width="1000px" height="500px">
+			<img src="http://localhost/test/dua.png" width="1000px" height="500px">
+			<img src="http://localhost/test/tiga.png" width="1000px" height="500px">
+			
+			
+			
+			</br>
+			<p> Pembayaran bisa melalui <b>Paypal</b>, <b>Bitcoin</b>, <b>DANA</b> 
+			<p>Jika Berminat silahkan Kontak Email: vipwarpsiwa@gmail.com </p>
+			
+			<div class="footer-payment-logo">
+<img src="http://localhost/test/paypal.png" /> <img src="http://localhost/test/bitcoin.png" /> <img src="http://localhost/test/dana.png" width="100px" height="60px">
+</div>
+          </div>
+      </div>
+      <div class="divider"></div>
+      <div class="row section">
+        <div class="col s12">
+          <ul class="horizontal-list">
+
+          </ul>
+        </div>
+      </div>      
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  </body>
+</html>
